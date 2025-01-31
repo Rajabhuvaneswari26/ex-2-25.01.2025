48abcadac95a6ec606a2ee8fdfa417c96d54dc67
Rajabhuvaneswari26 <rajabhuvaneswarig@gmail.com>
2025-01-31 16:15:54 +0530

    Add files via upload

---
+++ ex12/odd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n,rem;
+printf("Enter any integer:");
+scanf("%d",&n);
+
+if(rem %2==0)
+{
+printf("%d is even. \n",n);
+}
+else
+{
+printf("%d is odd. \n",n);
+}
+return 0;
+}
